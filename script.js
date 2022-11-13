282d61b48b10492b72c22019fcbe37c0ca72ab0b
Armin555ze <107627115+Armin555ze@users.noreply.github.com>
2022-11-13 15:25:53 +0100

    +

--- script.js
+++ script.js
@@ -101,4 +101,5 @@ document.querySelector('.again').addEventListener('click', function() {
    document.querySelector('.guess').value = '';
    document.querySelector('body').style.backgroundColor = '#222';
    document.querySelector('.number').style.width = '15rem';
+   document.querySelector('.naslov').textContent = 'Pogodi broj!';
 })
